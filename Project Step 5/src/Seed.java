875c3a2376c183150f38dc01c79ad87fa996db03
Athilah <athilah.st@gmail.com>
2025-06-25 17:13:42 +0700

    commit

--- Project Step 5/src/Seed.java
+++ Project Step 5/src/Seed.java
@@ -1,50 +1,38 @@
+
 import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
-/**
- * This enum is used by:
- * 1. Player: takes value of CROSS or NOUGHT
- * 2. Cell content: takes value of CROSS, NOUGHT, or NO_SEED.
- *
- * We also attach a display image icon (text or image) for the items.
- *   and define the related variable/constructor/getter.
- * To draw the image:
- *   g.drawImage(content.getImage(), x, y, width, height, null);
- *
- * Ideally, we should define two enums with inheritance, which is,
- *  however, not supported.
- */
-public enum Seed {   // to save as "Seed.java"
-    CROSS("X", "images/cross.gif"),   // displayName, imageFilename
-    NOUGHT("O", "images/not.gif"),
+
+public enum Seed {
+    CROSS("X", "images/cross_default.png"),
+    NOUGHT("O", "images/nought_default.png"),
     NO_SEED(" ", null);
 
-    // Private variables
     private String displayName;
-    private Image img = null;
+    private ImageIcon icon;
 
-    // Constructor (must be private)
-    private Seed(String name, String imageFilename) {
+    Seed(String name, String imageFilename) {
         this.displayName = name;
-
         if (imageFilename != null) {
-            URL imgURL = getClass().getClassLoader().getResource(imageFilename);
-            ImageIcon icon = null;
-            if (imgURL != null) {
-                icon = new ImageIcon(imgURL);
-                //System.out.println(icon);  // debugging
-            } else {
-                System.err.println("Couldn't find file " + imageFilename);
-            }
-            img = icon.getImage();
+            setImage(imageFilename);
         }
     }
 
-    // Public getters
+    public void setImage(String imageFilename) {
+
+        URL imgURL = getClass().getResource(imageFilename);
+        if (imgURL != null) {
+            this.icon = new ImageIcon(imgURL);
+        } else {
+            System.err.println("Gagal memuat skin: " + imageFilename);
+        }
+    }
+
     public String getDisplayName() {
         return displayName;
     }
+
     public Image getImage() {
-        return img;
+        return (icon != null) ? icon.getImage() : null;
     }
 }
